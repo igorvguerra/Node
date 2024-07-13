97da6c72e630c33d59853fb08a09d1071316deeb
Igor <igor.vguerra@hotmail.com>
2024-07-13 12:13:31 +0100

    creating the first function and second table

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trips";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
 
-app.get('/register', async () => {
-    await prisma.trip.create({
-        data: {
-            destination: 'Lisbon',
-            starts_at: new Date(),
-            ends_at: new Date(),
-        },
-    })
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-    return("Your register was sucessful.")
-});
-
-app.get('/list',  async() => {
-   const trips = await prisma.trip.findMany()
-    
-    return trips
-});
+app.register(createTrip)
 
 app.listen({ port: 3333}).then(() => {
     console.log('Server running!')
 });
+
